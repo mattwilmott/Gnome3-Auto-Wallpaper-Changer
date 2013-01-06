0aaad070c3a4505ea231bcf68e2982934108596e
Matthew Wilmott <mattwilmott@gmail.com>
2013-01-06 12:49:54 +1100

    Turns out the url had moved and only the US url provides access to the 1920x1200 images

--- bing-wallpaper-dl.sh
+++ bing-wallpaper-dl.sh
@@ -37,18 +37,22 @@ cd $DIR
 
 
 # Search through the following URLs for images
-bingUrls=(
- 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-au'
- 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-gb'
- 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-jp' )
+#bingUrls=(
+# 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-au'
+# 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-gb'
+# 'http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=8&mkt=en-jp' )
 
+ #http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US
  #http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US #appears broken
+#bingUrls=('http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=5&mkt=en-US')
+bingUrls=('http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=25&mkt=en-US')
+
 
 for url in ${bingUrls[@]}
 do
 	echo "Fetching Bing Url $url"
 	echo
-	temp=$(curl -s "$url" | xmlstarlet sel -t -m 'images/image/url' -v . -n)
+	temp=$(curl -s "$url" | xmlstarlet sel -t -m 'images/image/urlBase' -v '.' -n)
 	for i in $temp; do echo "Current image url: $i"; urls+=($i); done
 	echo "urls now contains ${#urls[@]} elements"
 	echo "Finished fetching $url"
@@ -58,14 +62,14 @@ if [[ "$DEBUG" ]]; then echo "Fetched XML docs, retrieving images"; fi
 for line in ${urls[@]}
 do
 	if [[ "$DEBUG" ]]; then echo "Current url:$line"; fi
-	# Convert hprichbg?p=rb%2fArafuneCoast_JA-JP1084689430.jpg to ArafuneCoast.jpg for instance
-	fileName="$(echo $line | cut -f2 -d'%' | cut -f1 -d'_' | sed 's/2f//').jpg"
+	# Convert hprichbg?p=rb%2fArafuneCoast_JA-JP1084689430 to ArafuneCoast.jpg for instance
+	fileName="$(echo $line | cut -f2 -d'%' | cut -f1 -d'_' | sed 's/2f//')_1920x1200.jpg"
 	if [[ "$DEBUG" ]]; then echo "filename determined as $fileName"; fi
 	if [[ -f $fileName ]]; then
 	    if [ "$DEBUG" ]; then echo "$fileName already exists"; fi
 	else
-	    if [[ "$DEBUG" ]]; then echo "Fetching image, $line"; fi
-	    curl -s http://www.bing.com/$line -o "$fileName";
+	    if [[ "$DEBUG" ]]; then echo "Fetching image, "$line"_1920x1200.jpg"; fi
+	    curl -s "http://www.bing.com/"$line"_1920x1200.jpg" -o "$fileName";
 	fi
 done
 echo "Finished"
